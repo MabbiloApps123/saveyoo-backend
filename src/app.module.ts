106616eef12a9857cbaf615a032edff23a67efe0
Anandroyal47 <Anandroyal147@gmail.com>
2024-12-10 19:13:45 +0530

    connections testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,24 +7,24 @@ import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from 'nestjs-schedule';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConfig } from './core/database/database.config';
+// import { databaseConfig } from './core/database/database.config';
 import { StoreModule } from './modules/store/store.module';
 
 const env = process.env.NODE_ENV || 'development';
-const config = databaseConfig[env];
-console.log(config);
+// const config = databaseConfig[env];
+// console.log(config);
 @Module({
   imports: [
     // DatabaseModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: config.host,
-      port: config.port,
-      username: config.username,
-      password: config.password,
-      database: config.database,
+      host: "dpg-ct9hjpm8ii6s73eckokg-a.singapore-postgres.render.com",
+      port: 5432,
+      username: "saveyoo_user",
+      password: "PquBCsonLPzN0otLLoEFHTTFANJzRHiV",
+      database: "saveyoo",
       entities: [`${__dirname}../../**/**.entity{.ts,.js}`],
-      ssl: false,
+      ssl: true,
       synchronize: true, // Consider using migrations instead
       extra: {
         connectionTimeoutMillis: 2000, // Timeout for connection
--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -1,67 +1,67 @@
-import { DataSource } from 'typeorm';
-import { DEVELOPMENT, TEST, PRODUCTION, STAGING, LOCAL } from '../constants';
-import { databaseConfig } from './database.config';
-import path from 'path';
-import * as glob from 'glob';
-import { logger } from '../utils/logger';
-import User from 'src/modules/users/entities/user.entity';
-import Otp from 'src/modules/users/entities/otp.entity';
+// import { DataSource } from 'typeorm';
+// import { DEVELOPMENT, TEST, PRODUCTION, STAGING, LOCAL } from '../constants';
+// import { databaseConfig } from './database.config';
+// import path from 'path';
+// import * as glob from 'glob';
+// import { logger } from '../utils/logger';
+// import User from 'src/modules/users/entities/user.entity';
+// import Otp from 'src/modules/users/entities/otp.entity';
 
-export let FRONTEND_BASE_URL: string;
+// export let FRONTEND_BASE_URL: string;
 
-export const databaseProviders = [
-  {
-    provide: DataSource,
-    useFactory: async () => {
-      const env = process.env.NODE_ENV || 'development';
-      const config = databaseConfig[env];
+// export const databaseProviders = [
+//   {
+//     provide: DataSource,
+//     useFactory: async () => {
+//       const env = process.env.NODE_ENV || 'development';
+//       const config = databaseConfig[env];
 
-      if (!config) {
-        throw new Error(`Database configuration missing for environment: ${env}`);
-      }
+//       if (!config) {
+//         throw new Error(`Database configuration missing for environment: ${env}`);
+//       }
 
-      logger.info(`Database configuration loaded for environment: ${env}`);
-      logger.info('Database_Config:', JSON.stringify(config));
+//       logger.info(`Database configuration loaded for environment: ${env}`);
+//       logger.info('Database_Config:', JSON.stringify(config));
 
-      const dataSource = new DataSource({
-        type: 'postgres',
-        host: config.host,
-        port: config.port,
-        username: config.username,
-        password: config.password,
-        database: config.database,
-        entities: [`${__dirname}/../**/**.entity{.ts,.js}`],
-        ssl:true,
-        synchronize: true, // Set to false in production
-        logging: env !== PRODUCTION,
-        // timezone: 'Z',
-      });
+//       const dataSource = new DataSource({
+//         type: 'postgres',
+//         host: config.host,
+//         port: config.port,
+//         username: config.username,
+//         password: config.password,
+//         database: config.database,
+//         entities: [`${__dirname}/../**/**.entity{.ts,.js}`],
+//         ssl:true,
+//         synchronize: true, // Set to false in production
+//         logging: env !== PRODUCTION,
+//         // timezone: 'Z',
+//       });
 
-      await testConnection(dataSource);
-      logger.info('Database connection initialized successfully.');
-      return dataSource;
-    },
-  },
-];
+//       await testConnection(dataSource);
+//       logger.info('Database connection initialized successfully.');
+//       return dataSource;
+//     },
+//   },
+// ];
 
-/**
- * Dynamically load all entity files.
- */
-export function getEntities() {
-  const entityFiles = glob.sync(path.resolve(__dirname, '../modules/**/*.entity.{ts,js}'));
-  const entities = entityFiles.map((file) => require(file).default);
-  return entities;
-}
+// /**
+//  * Dynamically load all entity files.
+//  */
+// export function getEntities() {
+//   const entityFiles = glob.sync(path.resolve(__dirname, '../modules/**/*.entity.{ts,js}'));
+//   const entities = entityFiles.map((file) => require(file).default);
+//   return entities;
+// }
 
-/**
- * Test the database connection.e
- */
-export async function testConnection(dataSource: DataSource) {
-  try {
-    await dataSource.initialize();
-    logger.info('Database connection established successfully.');
-  } catch (error) {
-    logger.error('Database connection error:', JSON.stringify(error));
-    throw error;
-  }
-}
+// /**
+//  * Test the database connection.e
+//  */
+// export async function testConnection(dataSource: DataSource) {
+//   try {
+//     await dataSource.initialize();
+//     logger.info('Database connection established successfully.');
+//   } catch (error) {
+//     logger.error('Database connection error:', JSON.stringify(error));
+//     throw error;
+//   }
+// }
