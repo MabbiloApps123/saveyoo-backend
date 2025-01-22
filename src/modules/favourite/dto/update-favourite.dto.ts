790f9297793d8fe4b64cbbd2d05600ab3c68eb12
Anandroyal47 <Anandroyal147@gmail.com>
2025-01-22 17:18:14 +0530

    issues fixed update dto

--- src/modules/favourite/dto/update-favourite.dto.ts
+++ src/modules/favourite/dto/update-favourite.dto.ts
@@ -1,7 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateFavouriteDto } from './create-favourite.dto';
 import { IsBoolean, IsOptional } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class UpdateFavouriteDto extends PartialType(CreateFavouriteDto) {
 
